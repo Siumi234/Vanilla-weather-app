9fbb35c8bb3af5ba7bd14bcb19ba17ec22971ab2
siumi <siumiwijekoon34@gmail.com>
2023-02-18 13:32:55 +0000

    changed api

--- src/script.js
+++ src/script.js
@@ -91,7 +91,8 @@ function displayTemperature(response) {
 }
 function searchCity(city) {
   let apiKey = "77d1da0a9341f4377306e3eb51b0c56f";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiUrl =
+    "https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric";
   axios.get(apiUrl).then(displayTemperature);
 }
 function handleSubmit(event) {

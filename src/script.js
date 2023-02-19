2a9310013dc8ef00bd6534ec8ea57be55742214c
siumi <siumiwijekoon34@gmail.com>
2023-02-19 09:23:17 +0000

    script updated

--- src/script.js
+++ src/script.js
@@ -60,9 +60,8 @@ function displayforecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "77d1da0a9341f4377306e3eb51b0c56f";
-
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&exclude={part}&appid=${apiKey}&units=metric`;
+  let apiKey = "ed238469f9b5e9d801834270e65449bc";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayforecast);
 }
 function displayTemperature(response) {
@@ -90,9 +89,8 @@ function displayTemperature(response) {
   getForecast(response.data.coord);
 }
 function searchCity(city) {
-  let apiKey = "77d1da0a9341f4377306e3eb51b0c56f";
-  let apiUrl =
-    "https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric";
+  let apiKey = "ed238469f9b5e9d801834270e65449bc";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
 function handleSubmit(event) {
